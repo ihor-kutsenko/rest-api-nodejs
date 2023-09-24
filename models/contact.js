101b21722d49a6f722b81514b88cd044d4a41bbe
Rammbest <igor-kutsenko@ukr.net>
2023-09-24 10:41:30 +0300

    update validation schema - name and email

--- models/contact.js
+++ models/contact.js
@@ -31,15 +31,12 @@ const contactSchema = new Schema(
 contactSchema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
-  name: Joi.string().required().messages({
+  name: Joi.string().min(3).max(30).required().messages({
     "any.required": "missing required name field",
   }),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required()
-    .messages({
-      "any.required": "missing required email field",
-    }),
+  email: Joi.string().email().required().messages({
+    "any.required": "missing required email field",
+  }),
   phone: Joi.string().required().messages({
     "any.required": "missing required phone field",
   }),
