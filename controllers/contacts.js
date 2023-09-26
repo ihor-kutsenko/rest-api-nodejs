9f99530c3c016a5bc6f02d39505987a449a51ff9
Rammbest <igor-kutsenko@ukr.net>
2023-09-26 22:17:19 +0300

    add filtered favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,14 +3,21 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
   }).populate("owner", " name email subscription");
 
-  res.json(result);
+  if (favorite) {
+    const filteredResult = result.filter((item) => {
+      return item.favorite === Boolean(favorite);
+    });
+    res.json(filteredResult);
+  } else {
+    res.json(result);
+  }
 };
 
 const getById = async (req, res) => {
